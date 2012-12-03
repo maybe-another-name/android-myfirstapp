1bd7900c496f6ec501f52f29c40eebb719a57714
firetongue <firstie.lastie@gmail.com>
2012-12-02 21:48:53 -0500

    End of my first app :(

--- MyFirstApp/src/com/firetongue/myfirstapp/MainActivity.java
+++ MyFirstApp/src/com/firetongue/myfirstapp/MainActivity.java
@@ -1,10 +1,15 @@
 package com.firetongue.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+
+	public static final String EXTRA_MESSAGE = "com.firetongue.myfirstapp.MESSAGE";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +22,18 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	/**
+	 * Intended to be called when the Send button is clicked.
+	 */
+	public void sendMessage(View view) {
+		// Open a new activity, and send it the text
+		Intent intent = new Intent(this, DisplayMessageActivity.class);
+		EditText editText = (EditText) findViewById(R.id.edit_message);
+		String message = editText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
 	}
 
 }
